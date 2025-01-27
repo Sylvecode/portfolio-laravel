1daadf1e92dc6fbd41e07a3e0562ee3cb1daccb8
Sylvain périé <sylvain.perie@hotmail.fr>
2025-01-27 17:38:07 +0100

    added portfolio card to my projects

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -28,8 +28,7 @@
                         Application mobile android calendrier des sorties cinéma, séries, jv, anime.
                     </p>
                     <div class="mt-4 mx-auto">
-                        <img src="{{ asset('images/screen_pixhub.jpg') }}"
-                        width="150" height="300"
+                        <img src="{{ asset('images/screen_pixhub.jpg') }}" width="150" height="300"
                             class="w-[150px] h-[300px] rounded-md border-solid border-2">
                     </div>
 
@@ -67,6 +66,33 @@
         </div>
         <div class="flex flex-row gap-10 justify-center">
             <div class="flex flex-row gap-10">
+                <a href="{{ url(app()->getLocale() . '/') }}"
+                    class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex flex-col motion-safe:hover:scale-[1.02] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 w-[700px] h-[500px]">
+                    <div class="h-16 w-full flex items-center">
+                        <div class="flex gap-4">
+                            <img src="{{ asset('images/logo_php.png') }}" alt="logo php" class="w-13 h-10">
+                            <img src="{{ asset('images/logo_laravel.svg') }}" alt="logo laravel" class="w-11 h-11">
+
+                        </div>
+                        <div class="flex ml-auto">
+                            <img src="{{ asset('images/logo_github.png') }}" alt="GitHub Logo"
+                                class="flex w-10 h-10 opacity-90">
+                        </div>
+                    </div>
+
+                    <h2 class="flex text-xl font-semibold text-gray-900 dark:text-white justify-center">sylvainperie.com
+                    </h2>
+                    <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed text-center">
+                        Mon portfolio en ligne
+                    </p>
+                    <div class="mt-4">
+                        <img src="{{ asset('images/screen_portfolio.png') }}"
+                            class="w-full h-[300px] object-cover rounded-md">
+                    </div>
+
+                </a>
+            </div>
+            <div class="flex flex-row gap-10 justify-center">
                 <a href="{{ url(app()->getLocale() . '/animalin') }}"
                     class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex flex-col motion-safe:hover:scale-[1.02] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 w-[700px] h-[500px]">
                     <div class="h-16 w-full flex items-center">
@@ -89,8 +115,11 @@
                     </div>
 
                 </a>
+
             </div>
-            <div class="flex flex-row gap-10 justify-center">
+        </div>
+        <div class="flex flex-row gap-10 justify-center">
+            <div class="flex flex-row gap-10">
                 <a href="{{ url(app()->getLocale() . '/frequencies') }}"
                     class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex flex-col motion-safe:hover:scale-[1.02] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 w-[700px] h-[500px]">
                     <div class="h-16 w-full flex items-center">
@@ -118,11 +147,8 @@
                     </div>
 
                 </a>
-
             </div>
-        </div>
-        <div class="flex flex-row gap-10 justify-center">
-            <div class="flex flex-row gap-10">
+            <div class="flex flex-row gap-10 justify-center">
                 <a href="{{ url(app()->getLocale() . '/easyupload') }}"
                     class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex flex-col motion-safe:hover:scale-[1.02] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 w-[700px] h-[500px]">
                     <div class="h-16 w-full flex items-center">
@@ -147,33 +173,32 @@
 
                 </a>
             </div>
-            <div class="flex flex-row gap-10 justify-center">
-                <a href="{{ url(app()->getLocale() . '/pokecodes') }}"
+        </div>
+        <div class="flex flex-col gap-10 justify-center">
+            <div class="flex flex-row gap-10 justify-center items-center">
+                <a href="{{ url(app()->getLocale() . '/') }}"
                     class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex flex-col motion-safe:hover:scale-[1.02] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 w-[700px] h-[500px]">
                     <div class="h-16 w-full flex items-center">
                         <div class="flex gap-4">
-                            <img src="{{ asset('images/logo_js.png') }}" loading="lazy" alt="logo js" class="w-13 h-11">
+                            <img src="{{ asset('images/logo_js.png') }}" alt="logo js" class="w-13 h-10">
                         </div>
                         <div class="flex ml-auto">
                             <img src="{{ asset('images/logo_github.png') }}" alt="GitHub Logo"
-                                class="w-10 h-10 opacity-90">
+                                class="flex w-10 h-10 opacity-90">
                         </div>
                     </div>
 
                     <h2 class="flex text-xl font-semibold text-gray-900 dark:text-white justify-center">PokéCodes
                     </h2>
                     <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed text-center">
-                        Bot Discord affichant les codes pokémon actuellement en distribution. Script JS de
+                        Bot Discord affichant les codes pokémon actuellement en distribution. Script Javascript de
                         webscrapping
                     </p>
-
-                    <div class="mt-4 mx-auto">
+                    <div class="flex mt-4 justify-center items-center">
                         <img src="{{ asset('images/screen_pokecodes.jpg') }}"
-                            class="w-[400px] h-[300px] object-cover rounded-md">
+                            class="flex justify-center w-[400px] h-[300px] object-cover rounded-md">
                     </div>
-
                 </a>
-
             </div>
         </div>
 </body>
