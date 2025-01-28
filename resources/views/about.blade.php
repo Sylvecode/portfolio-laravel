d99790bf83cdf4e892a421f614fcacd44536530b
Sylvain périé <sylvain.perie@hotmail.fr>
2025-01-28 16:41:31 +0100

    frontend dev

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'A propos')
 
 @section('content')
-<h2 class="text-black dark:text-white">Né dans l'idaho Oregon, Sylvain Périé a su déjoué les pronostics en s'imposant 2-8 contre Auxerre, ville connue pour ses diverses charrues, les festoches vieilles charrues qui sentent la pisse d'âne mal baté et j'en passe des vertes et des franchement pas mures...</h2>
-
+<div class="h-screen flex justify-center p-20 mt-20 m-20">
+<p class="text-black dark:text-white text-md">Après des études en vente puis en cadrage / montage vidéo, je me suis réorienté vers le developpement web en 2023 pour apprendre à créer des solutions digitales innovantes et ambitieuses dans un monde où le code est partout.<br>
+Je suis actuellement à la recherche d'une alternance ou un emploi à distance.</p>
+</div>
 @endsection
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,28 +3,39 @@
 @section('title', 'Contact')
 
 @section('content')
-<div class="mb-20 mt-50 flex justify-center flex-col text-black dark:text-white">
-    <h1 class="mx-auto p-10 mt-10 mb-4 text-xl">Vous pouvez me contacter par ici</h1>
+<div class="mb-20 mt-50 flex justify-center flex-row text-black dark:text-white">
+
 
     @if (session('success'))
         <p style="color: green;">{{ session('success') }}</p>
     @endif
 
-    <form action="{{ url(app()->getLocale() . '/send-mail') }}" method="POST"
-        class="mx-auto mb-40 p-7 w-[500px] bg-gray-100 border-shadow dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex flex-col transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-        @csrf
-        <label for="name" class="mb-2 text-lg">Nom</label>
-        <input type="text" class="mb-10 p-2 bg-gray-300 rounded text-black" id="name" name="name" required>
+    <div class="flex flex-col ">
+        <h1 class="mx-auto p-10 mt-10 mb-4 text-xl">Vous pouvez me contacter par ici</h1>
+        <form action="{{ url(app()->getLocale() . '/send-mail') }}" method="POST"
+            class="mx-auto mb-40 p-7 w-[500px] bg-gray-100 border-shadow dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex flex-col transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+            @csrf
+            <label for="name" class="mb-2 text-lg">Nom</label>
+            <input type="text" class="mb-10 p-2 bg-gray-300 rounded text-black" id="name" name="name" required>
 
-        <label for="email" class="mb-2 text-lg">E-mail</label>
-        <input type="email" class="mb-10 p-2 bg-gray-300 rounded text-black" id="email" name="email" required>
+            <label for="email" class="mb-2 text-lg">E-mail</label>
+            <input type="email" class="mb-10 p-2 bg-gray-300 rounded text-black" id="email" name="email" required>
 
-        <label for="message" class="mb-2 text-lg">Message</label>
-        <textarea id="message" name="message" rows="5" cols="30" class="p-2 bg-gray-300 mb-10 text-lg rounded text-black"
-            required></textarea>
+            <label for="message" class="mb-2 text-lg">Message</label>
+            <textarea id="message" name="message" rows="5" cols="30"
+                class="p-2 bg-gray-300 mb-10 text-lg rounded text-black" required></textarea>
 
-        <button type="submit" class="p-2 bg-blue-500 dark:bg-blue-500 w-30 mx-auto m-8 p-2 rounded text-lg ">Envoyer</button>
-    </form>
+            <button type="submit"
+                class="p-2 bg-blue-500 dark:bg-blue-500 w-30 mx-auto m-8 p-2 rounded text-lg ">Envoyer</button>
+        </form>
+    </div>
+    <div class="flex flex-row items-center m-20">
+        <h1 class="text-xl m-10">Ou par là : </h1>
+        <a href="mailto:sylvain.perie@hotmail.fr" class="text-xl">sylvain.perie@hotmail.fr</a>
+
+    </div>
+
+
 
 </div>
 @endsection
