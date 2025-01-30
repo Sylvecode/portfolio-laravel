f56370eee08006130dc7bc21d9ab57c63e0426d9
Sylvain périé <sylvain.perie@hotmail.fr>
2025-01-30 16:59:49 +0100

    test

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,6 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
